0e5133ea26268a6153ff15e514b1368e7e828e88
timur <timurturbill@gmail.com>
2024-07-11 14:52:03 +0300

    revert last commit

--- index.js
+++ index.js
@@ -19,43 +19,21 @@ app.get('/', (req, res) => {
 
 //GET
 app.get('/:url(*)', async (req, res) => {
-//    try {
-//        const url = req.params.url;
-//        const params = req.query;
-//        const headers = {
-//            "Content-Type": req.headers["content-type"],
-//            "Authorization": req.headers["authorization"]
-//        }
-//
-//        console.log({url, params, headers})
-//
-//        const response = await axios.get(url, {params: params, headers: headers});
-//        res.status(200).send(response.data);
-//    } catch (error) {
-//        res.status(400).send(error);
-//    }
+    try {
+        const url = req.params.url;
+        const params = req.query;
+        const headers = {
+            "Content-Type": req.headers["content-type"],
+            "Authorization": req.headers["authorization"]
+        }
 
-    let config = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: 'https://stageapi.trendyol.com/stagesapigw/suppliers/2738/orders',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic M3hBVENLNU9rUjZwOExCb2h2c2U6UXFmRlhpVWF0MXg4MkQ0dmRTYlM=',
-        'Cookie': '__cf_bm=WEJ5TjAP.0U7H_YADk_.Iqk9ftwd8.ST0Im9ACnMljk-1720696571-1.0.1.1-gB.sBNgGPLPCkxDNzzZUMWXTTSTEZYVqc8OR8pXgaTutxhwQ01k7GJyxDg7JmPWAZyn8tofZPaxr7jCgG69H1w; _cfuvid=WHmKQhscCBx0XMQVqqaTYIdPWS0.rHHWZqTuu732LgM-1720693763790-0.0.1.1-604800000'
-      }
-    };
+        console.log({url, params, headers})
 
-    axios.request(config)
-    .then((response) => {
-        console.log(response.data);
+        const response = await axios.get(url, {params: params, headers: headers, httpsAgent: agent});
         res.status(200).send(response.data);
-    })
-    .catch((error) => {
-        console.log(error);
+    } catch (error) {
         res.status(400).send(error);
-    });
-
+    }
 });
 
 //POST
